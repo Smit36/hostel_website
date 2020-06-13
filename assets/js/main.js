3a741e2c10d86a05792b003e331449bfc3a34b8d
unknown <smitgor1620@gmail.com>
2020-06-13 12:24:38 +0530

    Gallery button edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ var header = document.getElementById('myGallery');
 var btns = header.getElementsByClassName('gallery-btn');
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function () {
-    var current = document.getElementsByClassName('active');
+    var current = header.getElementsByClassName('active');
     current[0].className = current[0].classList.remove('active');
     this.className += ' active';
   });
