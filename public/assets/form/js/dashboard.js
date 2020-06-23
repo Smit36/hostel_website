5e66e59042d408e956f3a1a39316e0419d5b9b69
unknown <smitgor1620@gmail.com>
2020-06-23 16:37:38 +0530

    Dashboard created.

--- public/assets/form/js/dashboard.js
+++ public/assets/form/js/dashboard.js
@@ -1,55 +1,73 @@
-$(document).ready(function () {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyB4_9TFy63dBCct5Z1Y2n1m5zV8pTc_too',
-    authDomain: 'hshostel-798fe.firebaseapp.com',
-    databaseURL: 'https://hshostel-798fe.firebaseio.com',
-    projectId: 'hshostel-798fe',
-    storageBucket: 'hshostel-798fe.appspot.com',
-    appId: '1:687289260417:web:7518bf57b21911f1747080',
-    measurementId: 'G-5CQLG55K71',
-    messagingSenderId: '687289260417',
-  };
-  const f = firebase.initializeApp(firebaseConfig);
-  const db = f.firestore();
-  var i =0;
+const firebaseConfig = {
+  apiKey: 'AIzaSyB4_9TFy63dBCct5Z1Y2n1m5zV8pTc_too',
+  authDomain: 'hshostel-798fe.firebaseapp.com',
+  databaseURL: 'https://hshostel-798fe.firebaseio.com',
+  projectId: 'hshostel-798fe',
+  storageBucket: 'hshostel-798fe.appspot.com',
+  appId: '1:687289260417:web:7518bf57b21911f1747080',
+  measurementId: 'G-5CQLG55K71',
+  messagingSenderId: '687289260417',
+};
+const f = firebase.initializeApp(firebaseConfig);
+const db = f.firestore();
+var i = 0;
+document.getElementById('user_div').style.display = 'none';
+document.getElementById('login_div').style.display = 'block';
 
-  db.collection('enquiry')
-    .get()
-    .then((snapshot) => {
-      snapshot.docs.forEach((doc) => {
-        i++;
-        var firstName = doc.data().firstName;
-        var middleName = doc.data().middleName;
-        var lastName = doc.data().lastName;
-        var contact_no = doc.data().contact_no;
-        var father_contact = doc.data().father_contact;
-        var city = doc.data().city;
-        var district = doc.data().district;
-        var state = doc.data().state;
-        var reference = doc.data().reference;
-        
+db.collection('enquiry')
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      i++;
+      var firstName = doc.data().firstName;
+      var middleName = doc.data().middleName;
+      var lastName = doc.data().lastName;
+      var contact_no = doc.data().contact_no;
+      var father_contact = doc.data().father_contact;
+      var city = doc.data().city;
+      var district = doc.data().district;
+      var state = doc.data().state;
+      var reference = doc.data().reference;
 
-        $('#enquiry_table').append(
-          '<tr><td>'+i+'</td><td>' +
-            firstName +
-            ' ' +
-            middleName +
-            ' ' +
-            lastName +
-            '</td><td>' +
-            contact_no +
-            '</td><td>' +
-            father_contact +
-            '</td><td>' +
-            city +
-            ',' +
-            district +
-            ',' +
-            state +
-            '</td><td>' +
-            reference +
-            '</td></tr>',
-        );
-      });
+      if(reference==undefined){
+        reference="";
+      }
+
+      $('#enquiry_table').append(
+        '<tr><td>' +
+          i +
+          '</td><td>' +
+          firstName +
+          ' ' +
+          middleName +
+          ' ' +
+          lastName +
+          '</td><td>' +
+          contact_no +
+          '</td><td>' +
+          father_contact +
+          '</td><td>' +
+          city +
+          ',' +
+          district +
+          ',' +
+          state +
+          '</td><td>' +
+          reference +
+          '</td></tr>',
+      );
     });
-});
+  });
+
+var userValue = 'hsh123';
+var password = 'hsh123@';
+function login() {
+  var userEmail = document.getElementById('email_field').value;
+  var userPass = document.getElementById('password_field').value;
+  if (userEmail == userValue && userPass == password) {
+    document.getElementById('user_div').style.display = 'block';
+    document.getElementById('login_div').style.display = 'none';
+  } else {
+    alert('Please use correct username & password.');
+  }
+}
